c86678225d2e4d67b43f4d290a2fb37e6ea6e92f
cassiogvstos <cassiogvstos@gmail.com>
2018-06-13 14:12:51 -0300

    Itens removidos do model.user

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,7 +7,7 @@ const User = require ('../models/user');
 exports.get = (req, res, next) => {
 	User
 		// .find({active: true }, 'nome apelido data hora') O primeiro parâmetro do find passamos o que estiver ativo no banco
-														// já no segundo após a virgula estamo dizendo quais serão os campos mostrados
+		// já no segundo após a virgula estamo dizendo quais serão os campos mostrados
 		.find({}, 'nome apelido sexo data hora')												
 		.then(data => {
 			res.status(200).send(data);
